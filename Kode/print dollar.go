c452c5eef5e5651827119186c5702730dfda485f
mariusjt <31918600+mariusjt@users.noreply.github.com>
2018-02-05 15:37:53 +0100

    Add files via upload

---
+++ Kode/print dollar.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	hexverdi := []byte{'\x22', '\x20', '\xe2', '\x82', '\xac', '\x20', '\xc3', '\xb7', '\x20', '\xc2', '\xbe', '\x20', '\x64', '\x6f', '\x6c', '\x6c', '\x61', '\x72', '\x20', '\x22'}
+
+		fmt.Printf("%s\n", hexverdi)
+}
